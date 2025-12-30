014afac3d86973019fbd81903a48b56e06a0473d
Zzorro-Kid <5335395@email.com>
2025-12-30 13:58:50 +0200

    changes made 30.12

--- lib/features/home/presentation/pages/home_sceen.dart
+++ lib/features/home/presentation/pages/home_sceen.dart
@@ -41,7 +41,6 @@ class HomeScreen extends StatelessWidget {
   PreferredSizeWidget _buildAppBar() {
     return CustomAppBar(
       height: AppDimensions.appBarHeightHome,
-      topPadding: AppDimensions.appBarTopPadding,
       title: Transform.translate(
         offset: const Offset(0, AppDimensions.appBarTitleOffsetY),
         child: _buildAppBarContent(),
